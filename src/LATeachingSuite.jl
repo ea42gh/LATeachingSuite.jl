e9647acd4933f68244beee3c8c8345f9a6afcb54
ea42gh <ea42_github@mail.com>
2026-05-18 12:44:48 -0400

    Own curated umbrella bridge surface

--- src/LATeachingSuite.jl
+++ src/LATeachingSuite.jl
@@ -4,4 +4,35 @@ using Reexport
 
 @reexport using GenLAProblems
 
+module WorkflowDisplay
+
+using Reexport
+
+@reexport using GenLAProblems.WorkflowDisplay
+
+end
+
+module PythonBridge
+
+using Reexport
+
+@reexport using GenLAProblems.PythonBridge
+
+end
+
+load_LAFigureSpecs() = GenLAProblems.load_LAFigureSpecs()
+load_matrixlayout() = GenLAProblems.load_matrixlayout()
+la_version() = GenLAProblems.la_version()
+la_build() = GenLAProblems.la_build()
+ml_version() = GenLAProblems.ml_version()
+ml_build() = GenLAProblems.ml_build()
+show_svg(args...; kwargs...) = GenLAProblems.show_svg(args...; kwargs...)
+py_show_svg(args...; kwargs...) = GenLAProblems.py_show_svg(args...; kwargs...)
+l_show_svd(args...; kwargs...) = GenLAProblems.l_show_svd(args...; kwargs...)
+
+export WorkflowDisplay, PythonBridge
+export load_LAFigureSpecs, load_matrixlayout
+export la_version, la_build, ml_version, ml_build
+export show_svg, py_show_svg, l_show_svd
+
 end
