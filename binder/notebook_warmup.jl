14706ed2e3fd084553a4d814d3c49bbb44012034
ea42gh <ea42_github@mail.com>
2026-05-20 14:42:38 -0400

    Fix Binder ge_bundle warmup call

--- binder/notebook_warmup.jl
+++ binder/notebook_warmup.jl
@@ -18,7 +18,7 @@ show_backsubstitution!(pb; b_col=1, fig_scale=1.05)
 A_qr = gen_qr_problem(3; family=:pythagorean, maxint=2)
 qr_svg(A_qr)
 
-_svg_ge, ge_spec = ge_bundle(A_ge, B_ge[:, 1]; output_dir="/tmp/lat_ge")
+_svg_ge, ge_spec = ge_bundle(A_ge, B_ge[:, 1])
 display(ge_spec)
 
 U_svd, Sigma_svd, Vt_svd, A_svd = gen_svd_problem([2, 1], [2, 1], [3, 1, 0]; maxint=2)
