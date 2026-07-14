71e3eb9b730f7a7af1be6f442826b72242f1c43c
ea42gh <ea42_github@mail.com>
2026-07-14 18:53:25 -0400

    Verify canonical GE wrapper forwarding

--- test/test_wrapper_contracts.jl
+++ test/test_wrapper_contracts.jl
@@ -97,16 +97,22 @@ end
         callouts = [Dict("grid" => (0, 1), "label" => "A", "side" => "right")]
         decorations = [Dict("grid" => (0, 1), "rows" => (0, 0), "cols" => (0, 1), "background" => "yellow!35")]
         text_annotations = [Dict("grid_row" => 0, "text" => "\\qquad note")]
+        pivot_locs = [Dict("grid" => (0, 1), "entries" => [(0, 0)])]
+        rowechelon_paths = [Dict("grid" => (0, 1), "pivots" => [(0, 0)], "case" => "vv")]
         svg = LATeachingSuite.ge_svg(
             [[nothing, [1 0; 0 1]]];
             callouts=callouts,
             decorations=decorations,
             text_annotations=text_annotations,
+            pivot_locs=pivot_locs,
+            rowechelon_paths=rowechelon_paths,
         )
         @test svg isa LATeachingSuite.SVGOut
-        @test haskey(ge_seen, :callouts)
-        @test haskey(ge_seen, :decorations)
-        @test haskey(ge_seen, :text_annotations)
+        @test ge_seen[:callouts] == callouts
+        @test ge_seen[:decorations] == decorations
+        @test ge_seen[:text_annotations] == text_annotations
+        @test ge_seen[:pivot_locs] == pivot_locs
+        @test ge_seen[:rowechelon_paths] == rowechelon_paths
         @test !haskey(ge_seen, :specs)
         @test_throws ArgumentError LATeachingSuite.ge_svg([[nothing, [1 0; 0 1]]]; specs=callouts)
     finally
